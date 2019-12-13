d48f7def6da9e8aa08b9f04f2e864cc7fe2ebefc
Benjamin Maitland <marthinwurer@gmail.com>
2019-12-13 12:37:33 -0500

    Added an image list dataset for imagenet and custom datasets

--- dataset.py
+++ dataset.py
@@ -38,3 +38,27 @@ class MultiResolutionDataset(Dataset):
         img = self.transform(img)
 
         return img
+
+class ImageListDataset(Dataset):
+    def __init__(self, path, transform, resolution=8):
+        # load the paths from the text file
+        with open(path) as f:
+            image_paths = []
+            for line in f:
+                # good enough for now, make an issue if this fails.
+                if line.lower().endswith(('gif', 'jpeg', 'jpg',  'png')):
+                    image_paths.append(line.strip())
+
+        self.image_paths = image_paths
+        self.transform = transform
+        self.resolution = resolution
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        path = self.image_paths[index]
+        img = Image.open(path)
+        img = self.transform(img)
+
+        return img
